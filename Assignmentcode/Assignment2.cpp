a8aa055e58b848b484675474d73cbe5a8b6d676e
alaminkawsar <alaminkawsar8643@gmail.com>
2022-09-03 13:05:06 +0600

    half done

--- Assignmentcode/Assignment2.cpp
+++ Assignmentcode/Assignment2.cpp
@@ -1,3 +1,5 @@
+//matrix normal multiplication
+
 #include<bits/stdc++.h>
 #include <stdio.h>
 #include <mpi.h>
@@ -5,21 +7,32 @@
 using namespace std;
 
      
-int   main(int argc, char **argv)
-   {
-      
-      vector<int>mat1={{2,3,4},{1,2,3},{3,2, 1}};
-      
-      
-      ierr = MPI_Init(&argc, &argv);
+int   main(int argc, char **argv){
+   printf("Input The size of of 1st matrix:\n");
+   int n,m, p,q; cin>>n>>m;
+   int matrix1[n+5][m+5];
+   int matrix2[p+5][q+5];
 
-      /* find out MY process ID, and how many processes were started. */
-
-      ierr = MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
-      ierr = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-
-      printf("I'm process %i out of %i processes\n", 
-         my_id, num_procs);
-
-      ierr = MPI_Finalize();
+   printf("Input the first matrix:\n");
+   for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++)
+         cin>>matrix1[i][j];
    }
+
+   printf("Input the size of 2nd matrix");
+   cin>>p>>q;
+
+   printf("Input the first matrix:\n");
+   for(int i=0;i<p;i++){
+      for(int j=0;j<q;j++)
+         cin>>matrix2[i][j];
+   }
+
+   if(m!=p){
+      printf("Matrix mulplication can't be possible.
+      \nFirst matrix column and second matrix are must be same.\n
+      But found as %d and %d\n",m,p);
+   }
+
+   return 0;
+}
