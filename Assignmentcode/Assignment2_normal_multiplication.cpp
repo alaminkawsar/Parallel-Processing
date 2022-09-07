cb0b560d1158377f3374518fcdc24fddf06c441a
alaminkawsar <alaminkawsar8643@gmail.com>
2022-09-08 00:41:39 +0600

    normal matrix multiplication done

--- Assignmentcode/Assignment2_normal_multiplication.cpp
+++ Assignmentcode/Assignment2_normal_multiplication.cpp
@@ -2,16 +2,16 @@
 
 #include<bits/stdc++.h>
 #include <stdio.h>
-#include <mpi.h>
-
 using namespace std;
 
      
 int main(int argc, char **argv){
+
+   freopen("input.txt","r",stdin);
+
    printf("Input The size of of 1st matrix:\n");
    int n,m, p,q; cin>>n>>m;
    int matrix1[n+5][m+5];
-   int matrix2[p+5][q+5];
 
    printf("Input the first matrix:\n");
    for(int i=0;i<n;i++){
@@ -21,18 +21,40 @@ int main(int argc, char **argv){
 
    printf("Input the size of 2nd matrix");
    cin>>p>>q;
+   int matrix2[p+5][q+5];
 
-   printf("Input the first matrix:\n");
+   printf("Input the 2nd matrix:\n\n");
    for(int i=0;i<p;i++){
       for(int j=0;j<q;j++)
          cin>>matrix2[i][j];
    }
 
    if(m!=p){
-      printf("Matrix mulplication can't be possible.
-      \nFirst matrix column and second matrix are must be same.\n
-      But found as %d and %d\n",m,p);
+      printf("Matrix mulplication can't be possible.\n1st matrix column and 2nd matrix row values are must be same.\nBut found %d,%d\n",m,p);
+      return 0;
    }
+
+   //Now we will make a matrix with nxq dimention
+   int ansMatrix[n+5][q+5];
+   for(int k=0;k<n;k++){
+      for(int i=0;i<q;i++){
+         //m==p
+         ansMatrix[k][i]=0;
+         for(int j=0;j<m;j++){
+            ansMatrix[k][i]+=matrix1[k][j]*matrix2[j][i];
+         }
+      }
+   }
+   //show answer of multiplication
+   printf("Matrix Multiplication Result:\n");
+   m = q;
+   for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++){
+         printf("%d ",ansMatrix[i][j]);
+      }
+      cout<<endl;
+   }
+
 
    return 0;
 }
