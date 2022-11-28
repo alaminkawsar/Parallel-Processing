bfef54f67963c77bc341e928a097ae512e85953f
alaminkawsar <alaminkawsar8643@gmail.com>
2022-11-28 23:39:54 +0600

    reservation table related problem solved

--- Practice_Code/latency_transition_diagram.cpp
+++ Practice_Code/latency_transition_diagram.cpp
@@ -1,17 +1,4 @@
 #include<bits/stdc++.h>
-#define MAX 100005
-#define pb push_back
-#define mp make_pair
-#define pii pair<long long,long long>
-#define vi vector<int>
-#define vl vector<long long>
-#define read(a) scanf("%d",&a)
-#define p1(a) cout<<"Check "<<a<<endl;
-#define p2(a,b) cout<<"Check "<<a<<' '<<b<<endl;
-#define fo(i,n) for (i=0;i<n;i++)
-#define ll long long
-#define clr(a,b) memset(a,b,sizeof(a))
-#define MOD 1000000007
 
 using namespace std;
 int const sz=(int)2e5+5;
@@ -26,13 +13,10 @@ set<int> getForbiddenLatency(){
 
     for(int i=0;i<n;i++){
         m = s[i].size();
-        // cout << m << endl;
         for(int j=0;j<m;j++){
             if(s[i][j]=='1'){
-                //p2(i,j);
                 for(int k=j+1;k<m;k++){
                     if(s[i][k]=='1'){
-                        //p2(j,k);
                         forbidden.insert(abs(k-j));
                     }
                 }
@@ -66,8 +50,6 @@ vector<int>findPermissible(int n){
             permissible.push_back(i+1);
         }
     }
-
-
     return permissible;
 }
 
@@ -75,7 +57,7 @@ void Print(vector<int>v){
     for(int i=0;i<v.size();i++){
         cout<<v[i]<<' ';
     }
-    printf("\n");
+    printf("\n\n");
 }
 
 string getBinary(int n){
@@ -114,26 +96,20 @@ void Display(int ICV){
 
 void makeStateTransitionDiagram(string str_icv){
     int ICV = getDecimal(str_icv);
-    //cout<<str_icv<<endl;
-    //cout<<icv<<endl;
     queue<int>Q;
     Q.push(ICV);
     while(!Q.empty()){
         int parent_state = Q.front(); Q.pop();
         vector<int>permissible = findPermissible(parent_state);
-        //Print(permissible);
         for(auto permi: permissible){
             int child = ICV | (parent_state>>permi);
-
             if(has[{parent_state,child,permi}]) continue;
             has[{parent_state,child,permi}]=true;
-            
             adj[parent_state].push_back(make_pair(child,permi));
             Q.push(child);
         }
     }
     Display(ICV);
-
 }
 
 void solve()
@@ -159,7 +135,7 @@ void solve()
     for(auto it: permissible){
         printf("%d ",it);
     }
-    printf("\n");
+    printf("\n\n");
 
     /* TASK-3: Collision Vector */
     vector<int>collision(m+1,0);
@@ -170,7 +146,7 @@ void solve()
     for(int i=m;i>=1;i--){
         printf("%d ",collision[i]);
     }
-    printf("\n");
+    printf("\n\n");
 
     /*Task-4: Finding Collision Point*/
     queue<pair<int,int>>Q;
@@ -180,9 +156,6 @@ void solve()
             if(s[j][i]=='1') Q.push({j,i}),have[{j,i}]=true;
         }
     }
-    // for(auto x: have){
-    //     p2(x.first.first+1,x.first.second+1);
-    // }
     //check every initiation
     for(int i=0;i<m;i++){
         int x = 0;
@@ -203,21 +176,15 @@ void solve()
             for(auto it: store) have[it]=true;
         }
     }
-
-
     // Task-5: Draw State Transition Diagram
 
-    // make string using collision vector
     string str_icv;
     int icv=0,j=0;
     for(int i=m;i>=1;i--,j++){
         str_icv+=collision[i]+'0';
     }
-    //cout<<str_icv<<endl;
 
-    makeStateTransitionDiagram(str_icv);
-
-    
+    makeStateTransitionDiagram(str_icv);    
 }
 int main()
 {
