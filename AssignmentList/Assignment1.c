a0dbfc98179785f29fab2b200575ed9e1c7a31eb
alaminkawsar <alaminkawsar8643@gmail.com>
2022-11-28 20:30:51 +0600

    parallel sum added

--- AssignmentList/Assignment1.c
+++ AssignmentList/Assignment1.c
@@ -5,15 +5,15 @@ int   main(int argc, char **argv)
    {
       int ierr, num_procs, my_id;
 
-      ierr = MPI_Init(&argc, &argv);
+      MPI_Init(&argc, &argv);
 
       /* find out MY process ID, and how many processes were started. */
 
-      ierr = MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
-      ierr = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+      MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+      MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 
       printf("I'm process %i out of %i processes\n", 
          my_id, num_procs);
 
-      ierr = MPI_Finalize();
+      MPI_Finalize();
    }
---
+++ Practice_Code/MPI_Parallel_Sum.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+#include<mpi.h>
+using namespace std; 
+int n=10;
+int arr[]={1,2,3,4,5,6,7,8,9,10};
+int tempArr[100];
+int s;
+
+int main(int argc,char **argv){
+    int nprocess,pid,element_per_process,recieved_element;
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD,&nprocess);
+    MPI_Comm_rank(MPI_COMM_WORLD,&pid);
+
+    MPI_Status status;
+
+    if(pid==0){
+        int index, i;
+        element_per_process = n/nprocess;
+        for(i=1;i<nprocess-1;i++){
+            index = i*element_per_process;
+            MPI_Send(&element_per_process, 1 ,MPI_INT,i, 0, MPI_COMM_WORLD);
+            MPI_Send(&arr[index], element_per_process, MPI_INT,i, 0, MPI_COMM_WORLD);
+        }
+
+        // send last remaining element
+        index = i*element_per_process;
+        int remaining_element = n-index;
+        MPI_Send(&remaining_element, 1,MPI_INT,i, 0, MPI_COMM_WORLD);
+        MPI_Send(&arr[index], remaining_element, MPI_INT,i, 0, MPI_COMM_WORLD);
+        int sum = 0;
+
+        for(int i=0;i<element_per_process;i++){
+            sum+=arr[i];
+        }
+        printf("Summation from Master process: %d\n",sum);
+
+        int temp;
+        //receve from n number of process
+        for(int i=1;i<nprocess;i++){
+            MPI_Recv(&temp,1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+            printf("%d\n",temp);
+            sum+=temp;
+        }
+        
+        //print the total sum
+        printf("Toatal Sum From All Process: %d\n",sum);
+
+    }else{
+        MPI_Recv(&recieved_element,1,MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&tempArr,recieved_element, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        int temp=0;
+        for(int i=0;i<recieved_element;i++){
+            temp+=tempArr[i];
+        }
+        printf("Summation of Slave Process %d=%d\n",pid,temp);
+        MPI_Send(&temp, 1,MPI_INT,0, 0, MPI_COMM_WORLD);
+        
+    }
+
+    
+    MPI_Finalize();
+    
+    return 0;
+}
