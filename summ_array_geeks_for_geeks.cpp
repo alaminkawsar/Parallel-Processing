3cee3765cd15579d3ed4228121b8b39fe77680eb
alaminkawsar <alaminkawsar8643@gmail.com>
2022-11-28 23:52:08 +0600

    pthread summation

--- summ_array_geeks_for_geeks.cpp
+++ summ_array_geeks_for_geeks.cpp
@@ -1,121 +1,49 @@
-#include <mpi.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#define MAX 100
+// CPP Program to find sum of array
+#include <iostream>
+#include <pthread.h>
 
 // size of array
-#define n 9
+#define MAX 16
 
-int a[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+// maximum number of threads
+#define MAX_THREAD 4
 
-// Temporary array for slave process
-int a2[1000];
+using namespace std;
 
-int main(int argc, char* argv[])
+int a[] = { 1, 5, 7, 10, 12, 14, 15, 18, 20, 22, 25, 27, 30, 64, 110, 220 };
+int sum[4] = { 0 };
+int part = 0;
+
+void* sum_array(void* arg)
 {
 
- int pid, np,
-  elements_per_process,
-  n_elements_recieved;
- // np -> no. of processes
- // pid -> process id
+	// Each thread computes sum of 1/4th of array
+	int thread_part = part++;
 
- MPI_Status status;
+	for (int i = thread_part * (MAX / 4); i < (thread_part + 1) * (MAX / 4); i++)
+		sum[thread_part] += a[i];
+}
 
- // Creation of parallel processes
- MPI_Init(&argc, &argv);
+// Driver Code
+int main()
+{
 
- // find out process ID,
- // and how many processes were started
- MPI_Comm_rank(MPI_COMM_WORLD, &pid);
- MPI_Comm_size(MPI_COMM_WORLD, &np);
+	pthread_t threads[MAX_THREAD];
 
- // master process
- if (pid == 0) {
-  int index, i;
-  elements_per_process = n / np;
+	// Creating 4 threads
+	for (int i = 0; i < MAX_THREAD; i++)
+		pthread_create(&threads[i], NULL, sum_array, (void*)NULL);
 
-  // check if more than 1 processes are run
-  if (np > 1) {
-   // distributes the portion of array
-   // to child processes to calculate
-   // their partial sums
-   printf("Element per process: %d\n",elements_per_process);
-   for (i = 1; i < np - 1; i++) {
-    index = i * elements_per_process;
+	// joining 4 threads i.e. waiting for all 4 threads to complete
+	for (int i = 0; i < MAX_THREAD; i++)
+		pthread_join(threads[i], NULL);
 
-    MPI_Send(&elements_per_process,
-      1, MPI_INT, i, 0,
-      MPI_COMM_WORLD);
-    MPI_Send(&a[index],
-      elements_per_process,
-      MPI_INT, i, 0,
-      MPI_COMM_WORLD);
-   }
+	// adding sum of all 4 parts
+	int total_sum = 0;
+	for (int i = 0; i < MAX_THREAD; i++)
+		total_sum += sum[i];
 
-   // last process adds remaining elements
-   index = i * elements_per_process;
-   int elements_left = n - index;
+	cout << "sum is " << total_sum << endl;
 
-   MPI_Send(&elements_left,
-     1, MPI_INT,
-     i, 0,
-     MPI_COMM_WORLD);
-   MPI_Send(&a[index],
-     elements_left,
-     MPI_INT, i, 0,
-     MPI_COMM_WORLD);
-  }
-
-  // master process add its own sub array
-  int sum = 0;
-  for (i = 0; i < elements_per_process; i++)
-   sum += a[i];
-
-  // collects partial sums from other processes
-  int tmp;
-  for (i = 1; i < np; i++) {
-   MPI_Recv(&tmp, 1, MPI_INT,
-     MPI_ANY_SOURCE, 0,
-     MPI_COMM_WORLD,
-     &status);
-   int sender = status.MPI_SOURCE;
-
-   sum += tmp;
-  }
-
-  // prints the final sum of array
-  printf("Sum of array is : %d\n", sum);
- }
- // slave processes
- else {
-  MPI_Recv(&n_elements_recieved,
-    1, MPI_INT, 0, 0,
-    MPI_COMM_WORLD,
-    &status);
-
-  // stores the received array segment
-  // in local array a2
-  MPI_Recv(&a2, n_elements_recieved,
-    MPI_INT, 0, 0,
-    MPI_COMM_WORLD,
-    &status);
-
-  // calculates its partial sum
-  int partial_sum = 0;
-  for (int i = 0; i < n_elements_recieved; i++)
-   partial_sum += a2[i];
-   printf("Partial sum =%d from process %d\n",partial_sum,pid);
-
-  // sends the partial sum to the root process
-  MPI_Send(&partial_sum, 1, MPI_INT,
-    0, 0, MPI_COMM_WORLD);
- }
-
- // cleans up all MPI state before exit of process
- MPI_Finalize();
-
- return 0;
+	return 0;
 }
